971e2a1309747b140e3f27c0d3c91a92a4ba2a99
jingjielim <jingjie3000@gmail.com>
2021-03-16 23:19:42 +0800

    Use Map for resistor colorMap

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,46 +1,30 @@
 export class ResistorColor {
   private colors: string[];
-
+  private colorMap: Map<string, string> = new Map([
+    ["black", "0"],
+    ["brown", "1"],
+    ["red", "2"],
+    ["orange", "3"],
+    ["yellow", "4"],
+    ["green", "5"],
+    ["blue", "6"],
+    ["violet", "7"],
+    ["grey", "8"],
+    ["white", "9"],
+  ]);
   constructor(colors: string[]) {
     if (colors.length < 2)
       throw new Error("At least two colors need to be present");
 
     this.colors = colors;
   }
-  colorToResistance = (color: string): number =>{
-    switch (color) {
-      case "black":
-        return 0;
-      case "brown":
-        return 1;
-      case "red":
-        return 2;
-      case "orange":
-        return 3;
-      case "yellow":
-        return 4;
-      case "green":
-        return 5;
-      case "blue":
-        return 6;
-      case "violet":
-        return 7;
-      case "grey":
-        return 8;
-      case "white":
-        return 9;
-      default:
-        return 0;
-    }
-  }
+
   value = (): number => {
-    let resistance: number = 0;
-    for (let i = 0; i < 2;i++){
-      resistance = resistance * 10 + this.colorToResistance(this.colors[i]);
+    let resistance: string = "";
+    for (let i = 0; i < 2; i++) {
+      resistance += this.colorMap.get(this.colors[i]);
     }
 
-    return resistance;
+    return parseInt(resistance);
   };
-
-  
 }
